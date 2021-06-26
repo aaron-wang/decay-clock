4d2afdfb120f5008238d0cd281c91f6291982109
Muhammad Bin Ali <69480022+Muhammad-Bin-Ali@users.noreply.github.com>
2021-06-26 14:22:16 -0400

    Add files via upload

---
+++ src/Subpage/subpage_render.js
@@ -0,0 +1,6 @@
+const {ipcRenderer} = require("electron")
+
+let subpage = document.getElementById("subpage");
+subpage.addEventListener('click', () => {
+    ipcRenderer.send('subpage-task', 'page-two');
+}, false);
