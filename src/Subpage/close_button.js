c63a2bd55d5b062ea2f21165b981372265c7e4a9
aaron-wang <66176554+aaron-wang@users.noreply.github.com>
2021-07-16 18:13:42 -0400

    temp

--- src/Subpage/close_button.js
+++
@@ -1,8 +0,0 @@
-const electron = require('electron')
-const remote = electron.remote
-
-let btn = document.getElementById("close-pg");
-btn.addEventListener('click', () => {
-    var window = remote.getCurrentWindow();
-    window.close();
-})
--- src/Subpage/subpage_render.js
+++
@@ -1,7 +0,0 @@
-const {ipcRenderer} = require("electron")
-
-let subpage = document.getElementById("subpage");
-subpage.addEventListener('click', () => {
-    ipcRenderer.send('subpage-task', 'page-two');
-}, false);
-
